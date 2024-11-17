eefea089da3f98571bcb28fa4070d720953cf241
IsmaelDev13 <ismael-diaz8@hotmail.es>
2024-11-17 16:48:42 +0100

    validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 // import { SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  // const app = await NestFactory.create(AppModule);
-  // const options = new DocumentBuilder()
-  //   .setTitle('MongoDB Devices REST API')
-  //   .setDescription('API REST for showing devices con MongoDB')
-  //   .setVersion('1.0')
-  //   .build();
-
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://test-front-sage.vercel.app'],
+    origin: [
+      'http://localhost:3000',
+      'https://test-front-ismaeldev13s-projects.vercel.app/',
+    ],
     methods: ['GET', 'POST', 'DELETE', 'PUT'],
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization'],
   });
 
-  // const document = SwaggerModule.createDocument(app, options);
-  // SwaggerModule.setup('docs', app, document);
+  app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(process.env.PORT ?? 3000);
 }
