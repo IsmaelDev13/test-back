7ef24fc9a5f8f30f66bb983df48c3af3060a97fe
IsmaelDev13 <ismael-diaz8@hotmail.es>
2024-11-17 01:44:00 +0100

    device controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-
+import { Controller, Get, Req, Post, Param } from '@nestjs/common';
+import { Request } from 'express';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
@@ -8,5 +8,24 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+}
+
+@Controller('devices')
+export class DevicesController {
+  @Get()
+  findAll(@Req() request: Request): string {
+    return 'This action returns all devices';
+  }
+
+  @Post()
+  create(): string {
+    return 'This actions adds a new device';
+  }
+
+  @Get(':id')
+  findOne(@Param() params: any): string {
+    console.log(params.id);
+    return `This actions returns a #${params.id} device`;
   }
 }
