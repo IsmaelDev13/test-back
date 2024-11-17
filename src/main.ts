00308f7d741d9c49188ef76891f40f294b23b457
IsmaelDev13 <ismael-diaz8@hotmail.es>
2024-11-17 11:25:42 +0100

    remove cors

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+// import { SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const options = new DocumentBuilder()
-    .setTitle('MongoDB Devices REST API')
-    .setDescription('API REST for showing devices con MongoDB')
-    .setVersion('1.0')
-    .build();
+  // const app = await NestFactory.create(AppModule);
+  // const options = new DocumentBuilder()
+  //   .setTitle('MongoDB Devices REST API')
+  //   .setDescription('API REST for showing devices con MongoDB')
+  //   .setVersion('1.0')
+  //   .build();
 
-  const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('docs', app, document);
+  const app = await NestFactory.create(AppModule, { cors: false });
+  // const document = SwaggerModule.createDocument(app, options);
+  // SwaggerModule.setup('docs', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
